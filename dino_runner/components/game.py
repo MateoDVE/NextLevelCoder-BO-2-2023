cde7d326c7ce893bcc35ebcf70d22febf92691ec
MateoDVE <129457852+MateoDVE@users.noreply.github.com>
2023-04-03 22:09:41 -0400

    correccion

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -38,7 +38,7 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-        self.obstacle_manager.uptade(self)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
